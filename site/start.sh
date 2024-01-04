4335963133eb724674bb3b2231e066aaa90f8126
FakeBigBoi <nickernoose@gmail.com>
2024-01-04 03:04:34 -0500

    Update Gunicorn timeout in start.sh

--- site/start.sh
+++ site/start.sh
@@ -8,4 +8,4 @@ source env/bin/activate
 
 # Start Gunicorn for HTTP (port 80)
 sudo /home/dom/portfolio/site/env/bin/gunicorn --bind 0.0.0.0:80 main:app
-sudo /home/dom/portfolio/site/env/bin/gunicorn --bind 0.0.0.0:443 main:app --certfile=cert.pem --keyfile=privkey.pem
+sudo /home/dom/portfolio/site/env/bin/gunicorn --bind 0.0.0.0:443 main:app --certfile=cert.pem --keyfile=privkey.pem --timeout 60

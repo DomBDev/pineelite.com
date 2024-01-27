83493c56c2ee739d78d4c4ccdcdee98fd102434b
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-01-27 18:15:14 +0000

    Update Gunicorn startup scripts

--- site/start.sh
+++ site/start.sh
@@ -7,7 +7,11 @@ cd /home/dom/portfolio/site
 source env/bin/activate
 
 # Start Gunicorn for HTTP (port 80)
-sudo /home/dom/portfolio/site/env/bin/gunicorn --bind 0.0.0.0:80 -k gevent main:app &
+sudo /home/dom/portfolio/site/env/bin/gunicorn --bind 0.0.0.0:80 -k gevent main:app --log-level=info &
 
 # Start Gunicorn for HTTPS (port 443)
-sudo /home/dom/portfolio/site/env/bin/gunicorn --bind 0.0.0.0:443 -k gevent main:app --certfile=cert.pem --keyfile=privkey.pem --timeout 60 &
+sudo /home/dom/portfolio/site/env/bin/gunicorn --bind 0.0.0.0:443 -k gevent main:app --log-level=info --certfile=cert.pem --keyfile=privkey.pem --timeout 60 &
+
+
+
+

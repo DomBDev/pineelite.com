f19f090eaf2400afce4f6b8c74610ec0a955a37f
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-02-03 23:59:21 +0000

    Update socket connection to use HTTPS

--- site/static/scripts/security_feed.js
+++ site/static/scripts/security_feed.js
@@ -1,5 +1,5 @@
 // security_feed.js
-var socket = io.connect('http://' + document.domain + ':' + location.port);
+var socket = io.connect('http://' + window.location.hostname + ':' + location.port);
 var video = document.querySelector('video');
 
 socket.on('broadcast', function(data) {

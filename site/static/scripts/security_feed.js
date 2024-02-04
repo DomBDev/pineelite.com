737730b7c41f398f1a812bc43ed59d960512b32c
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-02-04 00:09:49 +0000

    Add MediaSource API support for video streaming

--- site/static/scripts/security_feed.js
+++ site/static/scripts/security_feed.js
@@ -2,10 +2,18 @@
 var socket = io.connect('https://' + window.location.hostname + ':' + location.port);
 var video = document.querySelector('video');
 
-socket.on('broadcast', function(data) {
-    if (video) {
-        video.src = data;
-    } else {
-        console.error('Video element not found');
-    }
-});
+if ('MediaSource' in window) {
+    var mediaSource = new MediaSource();
+    video.src = URL.createObjectURL(mediaSource);
+    mediaSource.addEventListener('sourceopen', sourceOpen);
+} else {
+    console.error('MediaSource API is not available in your browser');
+}
+
+function sourceOpen() {
+    var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+    socket.on('broadcast', function(data) {
+        var uint8Array = new Uint8Array(data);
+        sourceBuffer.appendBuffer(uint8Array);
+    });
+}

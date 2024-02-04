19eb0253a70607beb82f89b90f01d6f6193708ce
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-02-04 00:00:28 +0000

    Update socket connection to use HTTPS and handle missing video element

--- site/static/scripts/security_feed.js
+++ site/static/scripts/security_feed.js
@@ -1,7 +1,11 @@
 // security_feed.js
-var socket = io.connect('http://' + window.location.hostname + ':' + location.port);
+var socket = io.connect('https://' + window.location.hostname + ':' + location.port);
 var video = document.querySelector('video');
 
 socket.on('broadcast', function(data) {
-    video.src = data;
+    if (video) {
+        video.src = data;
+    } else {
+        console.error('Video element not found');
+    }
 });

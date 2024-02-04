dafcb941f8b051d2355418d22d0428a58c28a17d
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-02-04 01:09:45 +0000

    Refactor security_feed.js to use DOMContentLoaded event

--- site/static/scripts/security_feed.js
+++ site/static/scripts/security_feed.js
@@ -1,36 +1,42 @@
-// security_feed.js
-var socket = io.connect('wss://' + window.location.hostname + ':' + location.port, {
-    secure: true,
-    multiplex: false,
-    pingTimeout: 120000,
-    pingInterval: 5000
-});
-var video = document.querySelector('video');
-
-if ('MediaSource' in window) {
-    var mediaSource = new MediaSource();
-    video.src = URL.createObjectURL(mediaSource);
-    mediaSource.addEventListener('sourceopen', sourceOpen);
-} else {
-    console.error('MediaSource API is not available in your browser');
-}
-
-function sourceOpen() {
-    var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
-    socket.on('broadcast', function(arrayBuffer) {
-        if (sourceBuffer.updating || !arrayBuffer) return;
-
-        // Check for sourceBuffer in mediaSource.sourceBuffers
-        var isSourceBufferInBuffers = false;
-        for (let i = 0; i < mediaSource.sourceBuffers.length; i++) {
-            if (mediaSource.sourceBuffers[i] === sourceBuffer) {
-                isSourceBufferInBuffers = true;
-                break;
-            }
-        }
-        
-        if (!isSourceBufferInBuffers) return;
-        
-        sourceBuffer.appendBuffer(new Uint8Array(arrayBuffer));
+document.addEventListener('DOMContentLoaded', function() {
+    var socket = io.connect('wss://' + window.location.hostname + ':' + location.port, {
+        secure: true,
+        multiplex: false,
+        pingTimeout: 120000,
+        pingInterval: 5000
     });
-}
+    var video = document.querySelector('video');
+
+    if (!video) {
+        console.error('Video element not found');
+        return;
+    }
+
+    if ('MediaSource' in window) {
+        var mediaSource = new MediaSource();
+        video.src = URL.createObjectURL(mediaSource);
+        mediaSource.addEventListener('sourceopen', sourceOpen);
+    } else {
+        console.error('MediaSource API is not available in your browser');
+    }
+
+    function sourceOpen() {
+        var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+        socket.on('broadcast', function(arrayBuffer) {
+            if (sourceBuffer.updating || !arrayBuffer) return;
+
+            // Check for sourceBuffer in mediaSource.sourceBuffers
+            var isSourceBufferInBuffers = false;
+            for (let i = 0; i < mediaSource.sourceBuffers.length; i++) {
+                if (mediaSource.sourceBuffers[i] === sourceBuffer) {
+                    isSourceBufferInBuffers = true;
+                    break;
+                }
+            }
+            
+            if (!isSourceBufferInBuffers) return;
+            
+            sourceBuffer.appendBuffer(new Uint8Array(arrayBuffer));
+        });
+    }
+});

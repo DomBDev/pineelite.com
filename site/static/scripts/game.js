3a2245f4b7c26a7c16a0f890d04e4e95db0092a3
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-03-13 08:30:43 +0000

    Add sprite creation log statement

--- site/static/scripts/game.js
+++ site/static/scripts/game.js
@@ -59,6 +59,7 @@ var GameState = {
                     players[data.id]['last_update'] = new Date().getTime();
                 }
                 if (Object.keys(players[data.id]).includes('sprite') === false && this.own_sprite) {
+                    console.log("Creating sprite for: ", data.id)
                     players[data.id]['sprite'] = this.physics.add.sprite(data.location.x, data.location.y, 'player');
                     players[data.id]['sprite_text'] = this.add.text(data.location.x, data.location.y - 50, data.id, { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif' });
                     players[data.id]['sprite'].setScale(0.1);

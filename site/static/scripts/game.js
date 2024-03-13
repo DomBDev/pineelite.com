4b814cc63794da8e03bc0ce0d7759877987d00dd
Dominic <41173009+AstroDomDev@users.noreply.github.com>
2024-03-13 07:04:37 +0000

    Update console.log statement in game.js

--- site/static/scripts/game.js
+++ site/static/scripts/game.js
@@ -156,7 +156,7 @@ var GameState = {
         }
         if (playersToAdd.length > 0) {
         console.log("Players to add: ", playersToAdd)
-        console.log("Players: ", Object.keys(players))
+        console.log("Players: ", players)
         }
         for (var i = 0; i < playersToAdd.length; i++) {
             add_player(playersToAdd[i], this);
